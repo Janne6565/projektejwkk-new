d582326e4b5e276c93e26047165a95d29e3858b8
Janne Keipert <jabbekeipert@gmail.com>
2026-03-26 16:26:57 +0100

    style: reformat RootPage for consistent indentation and spacing

--- src/components/display/RootPage/RootPage.tsx
+++ src/components/display/RootPage/RootPage.tsx
@@ -2,19 +2,19 @@ import FloatingHeader from '@/components/display/FloatingHeader/FloatingHeader';
 import HeroSection from '@/components/display/HeroSection/HeroSection';
 import ProjectsSection from '@/components/display/ProjectsSection/ProjectsSection';
 import ModalRoot from '@/components/display/Modals/ModalRoot';
-import { ModalProvider } from '@/components/technical/modal-provider';
+import {ModalProvider} from '@/components/technical/modal-provider';
 
 const RootPage = () => {
-  return (
-    <ModalProvider>
-      <div className="bg-background w-screen transition-colors min-h-screen pb-500">
-        <FloatingHeader />
-        <HeroSection />
-        <ProjectsSection />
-        <ModalRoot />
-      </div>
-    </ModalProvider>
-  );
+    return (
+        <ModalProvider>
+            <div className="bg-background w-screen transition-colors min-h-screen">
+                <FloatingHeader/>
+                <HeroSection/>
+                <ProjectsSection/>
+                <ModalRoot/>
+            </div>
+        </ModalProvider>
+    );
 };
 
 export default RootPage;
